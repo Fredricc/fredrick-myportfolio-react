5cbd4f498618fb4e37d2e38aa499262551d8e759
Fredricc <75632511+Fredricc@users.noreply.github.com>
2024-08-01 05:32:35 +0300

    Update Education.js
    education update

--- src/Education.js
+++ src/Education.js
@@ -7,6 +7,17 @@ return (
     <div className="d-flex flex-column">
         <div className="education-one my-3 justify-content-between d-flex flex-row">
             <div className="d-flex flex-column new-one">
+                <span className="university-name">Moolya Ed and Techno Brain Ltd{" "}
+                </span>
+                <span className="university-degree">C#, Power Automate, .Net Playwright End to End Test Automation, Jenkins CI/CD and GIT certification <a href="https://moolyaed.certificate.givemycertificate.com/c/95c78b52-26cd-47c1-b4c1-6f906e402c38"> 95c78b52-26cd-47c1-b4c1-6f906e402c38</a>{" "}
+                </span>
+            </div>{" "}
+            <div>
+                <span className="year-passedout">2023</span>
+            </div>
+        </div>
+        <div className="education-one my-3 justify-content-between d-flex flex-row">
+            <div className="d-flex flex-column new-one">
                 <span className="university-name">Kenya Methodist University{" "}
                 </span>
                 <span className="university-degree">Bachelors of Science in Computer Information Systems{" "}
